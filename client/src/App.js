bed2a829d45fc384d70c7b555899c88cc41674d8
Geektimus <alexander.kno@gmail.com>
2018-07-04 15:27:59 -0500

    Add bootstrap and basic navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
+import AppNavbar from "./components/AppNavbar";
+
+import { Container } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <AppNavbar />
+        <Container>
+          <h1>Hello</h1>
+        </Container>
       </div>
     );
   }
